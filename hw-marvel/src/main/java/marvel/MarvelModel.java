832261d1bc5ab0c6ce6afdb35f0e489d0731fa8b
Yingfan Chen <yingfc@uw.edu>
2021-02-14 21:36:09 +0800

    add bean class

--- hw-marvel/src/main/java/marvel/MarvelModel.java
+++ hw-marvel/src/main/java/marvel/MarvelModel.java
@@ -1,2 +1,27 @@
-package marvel;public class MarvelModel {
+package marvel;
+
+import com.opencsv.bean.CsvBindByName;
+
+public class MarvelModel {
+    @CsvBindByName
+    private String hero;
+
+    @CsvBindByName
+    private String book;
+
+    public String getHero() {
+        return this.hero;
+    }
+
+    public String getBook() {
+        return this.book;
+    }
+
+    public void setHero(String v) {
+        this.hero = v;
+    }
+
+    public void setBook(String v) {
+        this.book = v;
+    }
 }
