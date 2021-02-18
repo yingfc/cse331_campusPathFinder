4a7af2d0b9662cbe76d50ab0c6cd44aff5899432
Andrew Gies <agies@cs.washington.edu>
2021-02-18 12:42:00 -0800

    Staff push of files

---
+++ hw-pathfinder/src/main/java/pathfinder/CampusMap.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2021 Hal Perkins.  All rights reserved.  Permission is
+ * hereby granted to students registered for University of Washington
+ * CSE 331 for use solely during Winter Quarter 2021 for purposes of
+ * the course.  No other use, copying, distribution, or modification
+ * is permitted without prior written consent. Copyrights for
+ * third-party components of this work must be honored.  Instructors
+ * interested in reusing these course materials should contact the
+ * author.
+ */
+
+package pathfinder;
+
+import pathfinder.datastructures.Path;
+
+import java.util.Map;
+
+public class CampusMap implements ModelAPI {
+
+    @Override
+    public boolean shortNameExists(String shortName) {
+        // TODO: Implement this method exactly as it is specified in ModelAPI
+        throw new RuntimeException("Not Implemented Yet");
+    }
+
+    @Override
+    public String longNameForShort(String shortName) {
+        // TODO: Implement this method exactly as it is specified in ModelAPI
+        throw new RuntimeException("Not Implemented Yet");
+    }
+
+    @Override
+    public Map<String, String> buildingNames() {
+        // TODO: Implement this method exactly as it is specified in ModelAPI
+        throw new RuntimeException("Not Implemented Yet");
+    }
+
+    @Override
+    public Path findShortestPath(String startShortName, String endShortName) {
+        // TODO: Implement this method exactly as it is specified in ModelAPI
+        throw new RuntimeException("Not Implemented Yet");
+    }
+
+}
