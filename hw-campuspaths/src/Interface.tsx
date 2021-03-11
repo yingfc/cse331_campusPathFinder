35fe2def144f68e7896287531b86f12437738131
Yingfan Chen <yingfc@uw.edu>
2021-03-11 13:49:21 +0800

    add documentation to Path interface

--- hw-campuspaths/src/Interface.tsx
+++ hw-campuspaths/src/Interface.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 
 export interface Point {
-    x: number;
-    y: number;
+    x: number;  // the x-coordinate of a point
+    y: number;  // the y=coordinate of a point
 }
 
 export interface Segment {
-    start: Point;
-    end: Point;
-    cost: number;
+    start: Point;   // the start point of a segment
+    end: Point;     // the end point of a segment
+    cost: number;   // the cost of a segment
 }
 
 export interface Path {
-    start: Point;
-    path: Segment[];
+    start: Point;       // the start point of a path
+    path: Segment[];    // the actual path of list of segments
 }
